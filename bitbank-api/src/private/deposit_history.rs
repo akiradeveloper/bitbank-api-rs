5b435677a506c5120672ad53300f607f55d4842c
Akira Hayakawa <ruby.wktk@gmail.com>
2023-04-16 11:01:46 +0900

    Add deposit-history API

---
+++ bitbank-api/src/private/deposit_history.rs
@@ -0,0 +1,70 @@
+use super::*;
+
+#[derive(strum::EnumString, strum::Display, Debug)]
+#[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
+pub enum Status {
+    Found,
+    Confirmed,
+    Done,
+}
+
+#[serde_as]
+#[derive(Deserialize)]
+pub struct Deposit {
+    uuid: String,
+    #[serde_as(as = "DisplayFromStr")]
+    asset: Asset,
+    amount: f64,
+    /// Exists only for crytos.
+    txid: Option<String>,
+    #[serde_as(as = "DisplayFromStr")]
+    status: Status,
+    #[serde_as(as = "TimestampMilliSeconds")]
+    found_at: NaiveDateTime,
+    /// Exists only for confirmed ones.
+    #[serde_as(as = "Option<TimestampMilliSeconds>")]
+    confirmed_at: Option<NaiveDateTime>,
+}
+
+#[derive(Deserialize)]
+struct Response {
+    deposits: Vec<Deposit>,
+}
+
+#[serde_as]
+#[derive(TypedBuilder, Serialize, Debug)]
+pub struct Params {
+    #[serde_as(as = "DisplayFromStr")]
+    asset: Asset,
+    #[builder(default, setter(strip_option))]
+    count: Option<u8>,
+    #[builder(default, setter(strip_option))]
+    #[serde_as(as = "Option<TimestampMilliSeconds>")]
+    since: Option<NaiveDateTime>,
+    #[builder(default, setter(strip_option))]
+    #[serde_as(as = "Option<TimestampMilliSeconds>")]
+    end: Option<NaiveDateTime>,
+}
+
+pub async fn get(cred: Credential, params: Params) -> anyhow::Result<Vec<Deposit>> {
+    let resp: Response = ApiExec { cred }
+        .get("/v1/user/spot/deposit_history", to_query_params(params)?)
+        .await?;
+    Ok(resp.deposits)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_params() -> anyhow::Result<()> {
+        let params = Params::builder()
+            .asset(XRP)
+            .since(NaiveDateTime::from_timestamp_opt(10, 0).unwrap())
+            .count(100)
+            .build();
+        assert_eq!(to_query_params(params)?, "asset=xrp&count=100&since=10000");
+        Ok(())
+    }
+}
